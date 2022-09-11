674adc882507339afee20134eaefbb9c3447c903
Anirudh Kumar <51256968+Skylur-kross@users.noreply.github.com>
2022-09-11 17:30:20 +0530

    Add files via upload

--- Food_app.js
+++ Food_app.js
@@ -2,26 +2,45 @@ let addButtons = document.querySelectorAll(".add");
 let removeButtons = document.querySelectorAll(".remove");
 
 const ITEMS = [
-  "bhaji.5",
-  "bhaji1",
-  "pav",
-  "bhaji2",
+  {
+    item_name: 'Bhaji (500 Gms) (2-3 People)',
+    item_ID: 0
+  },
+  {
+    item_name: 'Bhaji 1 Kg (4-5 People)',
+    item_ID: 1
+  },
+  {
+    item_name: 'Special Pav Bhaji + Masala Pav',
+    item_ID: 2
+  },
+  {
+    item_name: 'Missal PAV',
+    item_ID: 3
+  },
 ];
 
-let CART = {};
+let CART = {
+  
+};
+
+
+
 
 addButtons
   .forEach((btn, index) => btn.addEventListener('click',
     function () {
       const index = Array.from(addButtons).indexOf(btn);
-      if (CART[ITEMS[index]]) {
-        CART[ITEMS[index]]++;
+      if (CART.hasOwnProperty(ITEMS[index].item_name)) {
+        CART[ITEMS[index].item_name]++;
       } else {
-        CART[ITEMS[index]] = 1;
+        CART[ITEMS[index].item_name] = 1;
       }
       // console.log(CART)
       resetCart()
       updateCart(CART);
+      document.getElementsByClassName("CartItem").style.padding = "16px";
+      document.getElementsByClassName("CartValue").style.color = "red";
     }, false)
   );
 
@@ -30,12 +49,14 @@ removeButtons
     function () {
       const index = Array.from(removeButtons).indexOf(btn);
 
-      if (CART[ITEMS[index]]) {
-        if (CART[ITEMS[index]] >= 0) {
-          CART[ITEMS[index]]--;
+      if (CART.hasOwnProperty(ITEMS[index].item_name)) {
+        if (CART[ITEMS[index].item_name]) {
+          CART[ITEMS[index].item_name]--;
+        } else {
+          delete CART[ITEMS[index].item_name];
         }
       } else {
-        CART[ITEMS[index]] = 0;
+        CART[ITEMS[index].item_name] = 0;
       }
       // console.log(CART)
       resetCart()
@@ -47,30 +68,60 @@ removeButtons
 function updateCart(CART) {
   const myTableBody = document.getElementById("carttable");
 
-  const cartkeys =Object.keys(CART);
-  
+  // const helloWorlds = {
+  //   spanish: '¡Hola Mundo!',
+  //   polish: 'Witaj świecie!',
+  //   french: 'Bonjour le monde!'
+  // }
+
+  // const helloWorldsKeys = Object.keys(helloWorlds);
+
+  // for (let i = 0; i < helloWorldsKeys.length; i++) {
+
+  //   // create a table row element
+  //   let tableRow = document.createElement('tr');
+
+  //   // create a table cell (td) element
+  //   let listItem = document.createElement('td');
+  //   listItem.classList.add('listItem');
+
+  //   // add content to table cell element
+  //   listItem.innerHTML = helloWorlds[helloWorldsKeys[i]];
+
+  //   // append table cell to table row
+  //   tableRow.appendChild(listItem);
+
+  //   // append table row to table body
+  //   myTableBody.appendChild(tableRow);
+  // }
+  // console.log(Object.keys(CART));
+  const cartkeys = Object.keys(CART);
+  // for (let i = 0; i < cartkeys.length; i++) {
+  cartkeys.forEach((itemKey) => {
+
     let tableRow1 = document.createElement('tr');
 
     let cartItem = document.createElement('td');
     cartItem.classList.add('CartItem');
 
-    let tableRow2 = document.createElement('tr');
-
     let cartValue = document.createElement('td');
     cartValue.classList.add('CartValue');
 
-    cartItem.innerHTML = Object.keys(CART);
-    cartValue.innerHTML = Object.values(CART);
+    cartItem.innerHTML = itemKey;
+    cartValue.innerHTML = CART[itemKey];
     // console.log(Object.keys(CART)[i])
     tableRow1.appendChild(cartItem);
-    tableRow2.appendChild(cartValue);
+    tableRow1.appendChild(cartValue);
+
 
     myTableBody.appendChild(tableRow1);
-    myTableBody.appendChild(tableRow2);
-  }
+    
 
-  function resetCart(){
-    const myNode = document.getElementById("carttable");
-    myNode.innerHTML = '';
-  }
+  });
+}
+
+function resetCart() {
+  const myNode = document.getElementById("carttable");
+  myNode.innerHTML = '';
+}
 
